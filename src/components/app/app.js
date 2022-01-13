c59fbcdad2b33175396d39c000084dfac138665f
Aleksey Osipov <oad2509@mail.ru>
2022-01-13 23:53:38 +0300

    Init project

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import './app.scss';
+import AppHeader from "../appHeader/appHeader";
+
+function App() {
+    return (
+        <div className="app">
+            <AppHeader/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/appHeader/appHeader.js
@@ -0,0 +1,18 @@
+import './appHeader.scss'
+
+const AppHeader = () =>{
+    return(
+        <header className="app__header">
+            <h1 className="app__title"><span>Marvel</span> information portal</h1>
+            <nav className="app__menu">
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    /
+                    <li><a href="#">Comics</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default AppHeader;
